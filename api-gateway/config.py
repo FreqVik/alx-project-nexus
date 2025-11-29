deac95c7f1633c2a8759a2b36f7edc1fb8bfa7e4
VikingVik123 <vodimmegwa@gmail.com>
2025-11-29 13:41:05 +0100

    updated gateway

--- api-gateway/config.py
+++ api-gateway/config.py
@@ -1,10 +1,11 @@
 from pydantic_settings import BaseSettings
 
 
+
 class Settings(BaseSettings):
-    user_service_url: str = "http://127.0.0.1:5000"
-    poll_service_url: str = "http://127.0.0.1:7000"
-    vote_service_url: str = "http://127.0.0.1:8000"
+    user_service_url: str 
+    poll_service_url: str
+    vote_service_url: str
 
     class Config:
         env_file = ".env"
