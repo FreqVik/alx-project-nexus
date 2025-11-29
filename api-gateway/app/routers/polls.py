75a96ffc39b1d5a4c07106ec918fb96bf88677e5
VikingVik123 <vodimmegwa@gmail.com>
2025-11-29 01:54:49 +0100

    updated gateway

--- api-gateway/app/routers/polls.py
+++ api-gateway/app/routers/polls.py
@@ -1,12 +1,32 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
+from pydantic import BaseModel
+from typing import List, Literal
+from fastapi.security import OAuth2PasswordBearer
 from ..proxy import reverse_proxy
 from config import settings
 
 router = APIRouter()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/token")
+
+class PollOption(BaseModel):
+    text: str
+    order: int
+
+class PollCreateBody(BaseModel):
+    title: str
+    description: str
+    author_id: str
+    type: Literal["single", "multiple"]
+    status: Literal["draft", "published", "closed"]
+    allow_anonymous: bool
+    allow_change_vote: bool
+    max_choices: int
+    options: List[PollOption]
+
 
 @router.post("/")
-async def create_poll(request: Request):
+async def create_poll(request: Request, body: PollCreateBody, token: str = Depends(oauth2_scheme)):
     return await reverse_proxy(settings.poll_service_url, request)
 
 
--- api-gateway/main.py
+++ api-gateway/main.py
@@ -1,11 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer
 from app.routers import users, polls, votes
 
 app = FastAPI(title="API Gateway")
 
 # Include the routers for each service
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/token")
+
 app.include_router(users.router, prefix="/users", tags=["User Service"])
-app.include_router(polls.router, prefix="/polls", tags=["Poll Service"])
+app.include_router(polls.router, prefix="/polls", tags=["Poll Service"], dependencies=[Depends(oauth2_scheme)])
 app.include_router(votes.router, prefix="/votes", tags=["Vote Service"])
 
 @app.get("/")
